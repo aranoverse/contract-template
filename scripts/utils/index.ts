fc20b0ec0f963da4fcb1a15d09dd2c68e4210284
Arano <aranoverse@outlook.com>
2023-12-04 10:58:48 +0800

    feat: contract naming

--- scripts/utils/index.ts
+++ scripts/utils/index.ts
@@ -1,3 +1,16 @@
+import {BigNumber} from "ethers"
+
 export async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export function bigNumber(num: string | number, decimals: number = 18): BigNumber {
+  return BigNumber.from(10).pow(decimals).mul(BigNumber.from(num))
+}
+
+export function capitalizeFirstLetter(input: string): string {
+  if (input.length === 0) {
+    return input;
+  }
+  return input.charAt(0).toLowerCase() + input.slice(1);
 }
